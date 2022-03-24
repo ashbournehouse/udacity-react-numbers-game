5bf2721defd211dda2c317b8b4c18832fc5b2454
Andrew <a.biller@ashho.com>
2022-03-24 18:49:16 +0000

    Add makeNewvaluesArray method

--- src/Components/game.jsx
+++ src/Components/game.jsx
@@ -12,29 +12,40 @@ class Game extends React.Component {
   // We don't seem to be able to use the 'direct' way here so ...
   constructor(props) {
     super(props);
-    
+    /*
     const value1 = Math.floor(Math.random() * 100);
     const value2 = Math.floor(Math.random() * 100);
     const value3 = Math.floor(Math.random() * 100);
-    const proposedAnswer = (Math.floor(Math.random() * 3) + value1 + value2 + value3);
+    */
+    //Replace the bit above with an array ..easier to pass around!
+    const valuesArray = this.makeNewValuesArray();
     const numQuestions = 0;
     const numCorrect = 0;
     
     this.state = {
-      value1: value1,
-      value2: value2,
-      value3: value3,
-      proposedAnswer: proposedAnswer,
+      valuesArray: valuesArray,
       numQuestions: numQuestions,
       numCorrect: numCorrect,
     };
   }
+  makeNewValuesArray = () => {
+    var tempArray = [];
+    var tempSum = 0;
+    for(var i=0; i < 3; i++){
+      const value = Math.floor(Math.random() * 100)
+      tempArray.push(value);
+      tempSum += value   
+    }
+    tempArray.push(Math.floor(Math.random() * 3) + tempSum);
+    return tempArray;    
+  }
+
   render() {
     return(
       <div className="game">
         <h2>Mental Math</h2>
         <div className="equation">
-          <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
+          <p className="text">{`${this.state.valuesArray[0]} + ${this.state.valuesArray[1]} + ${this.state.valuesArray[2]} = ${this.state.valuesArray[3]}`}</p>
         </div>
         <button>True</button>
         <button>False</button>
